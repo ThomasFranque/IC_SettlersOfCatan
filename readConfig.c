838ca576dc8bba69d334b1a0743aeb87c62e2509
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-01-19 12:46:49 +0000

    KErmitSiDE

--- readConfig.c
+++ readConfig.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void set_config_val(CONFIG *config, const char *key, int val) {
+void set_config_val(MAP_CONFIG *config, const char *key, int val) {
 	/**
 	Esta funcao atribui a uma estrutura CONFIG o valor x e y do board
-	@param config = estrutura tipo CONFIG para xdim e ydim
+	@param config = estrutura tipo MAP_CONFIG para xdim e ydim
 	@param Key = palavra chave do que se le da INI
 	@param val = valor que se vai atribuir a chave correspondente na estrutura
 	*/
@@ -27,24 +27,34 @@ void set_unit_val(UNIT *map_unit, const char *key, int val){
 	@param Key = palavra chave do que se le da INI
 	@param val = valor que se vai atribuir a chave correspondente na estrutura
 	*/
-	if (map_unit ==NULL)
+	if (map_unit ==NULL)/**Presumo que isto de erro pois As posicoes sao strings e nao sei se isto mete como apontador se nao ¯\_(?)_/¯*/
 		return;
 	if (strcmp(key, "N")== 0)
 		map_unit->N = val
+	else if (strcmp(key, "S") == 0)
+		map_unit->S = val
+	else if (strcmp(key, "W") == 0)
+		map_unit->W = val
+	else if (strcmp(key, "E") == 0)
+		map_unit->E = val
+	else
+		;
 }
 
-void read_config(CONFIG *config, UNIT * map_unit, FILE *f) {
+void read_config(MAP_CONFIG *config,FILE *f) {
 	/**
 	Esta funcao e onde de facto os valores vao ser atribuidos e procurados, quando se quer ler um ficheiro INI so e necessario
 	chamar esta funcao com as estruturas pedidas.
-	@param config = estrutura tipo CONFIG para xdim e ydim
+	@param config = estrutura tipo MAP_CONFIG para xdim e ydim
 	@param map_unit = Uma estrutura tipo UNIT para guardar os valores das 4 direcoes a volta lidas da INI
 	@param f = ficheiro INI de onde se vao ler os valores para atribuir as estruturas passadas a funcao
 	*/
 	char str[MAX];
 	char *token;
 	const char *delim = "=\n";
-
+	int counter = 0;
+	MAP_CONFIG *world;
+	
 	while (fgets(str, MAX, f) != NULL) {
 		if (strchr(str, ";"))
 			;
@@ -58,8 +68,29 @@ void read_config(CONFIG *config, UNIT * map_unit, FILE *f) {
 			token = strtok(NULL, delim);
 			value = atoi(token);
 
-			set_config_val(config, varname, value);
+			set_config_val(world, varname, value);
+		}
+	}
+	
+	world = malloc(sizeof(MAP_CONFIG));
+	world->grid = calloc(world.xdim * world.ydim, sizeof(UNIT));
+	
+	while (fgets(str, MAX, f) != NULL) {
+		if (strchr(str, ";"))
+			;
+		else if (strchr(str, "=")) {
+			char varname[MAX];
+			int value;
+
+			token = strtok(str, delim);
+			strcpy(varname, token);
+
+			token = strtok(NULL, delim);
+			value = atoi(token);
 			
+			if (strcmp(varname, "N")==0)
+				counter += 1;
+			set_unit_val(world.grid[counter -1], varname, value);
 		}
 	}
 	fclose(f);
--- readconfig.h
+++ readconfig.h
@@ -5,13 +5,8 @@
 #define READCONFIG_H_INCLUDED
 #define MAX 128
 
-typedef struct CONFIGURATION {
-	unsigned int xdim;
-	unsigned int ydim;
-} CONFIG;
-
-typedef struct MAP_UNIT{ 
-/**Estrotura da unidade do mapa, primeira posicao de cada array é uma letra, o resto um numero*/
+typedef struct { 
+/**Estrutura da unidade do mapa, primeira posicao de cada array é uma letra, o resto um numero*/
 	char N[4];
 	char S[4];
 	char W[4];
@@ -19,7 +14,16 @@ typedef struct MAP_UNIT{
 	char Building;
 }UNIT;
 
+typedef struct{
+	UNIT *grid;
+	unsigned int xdim;
+	unsigned int ydim;
+} MAP_CONFIG;
+
+
+
 void set_config_val(CONFIG *config, UNIT *map_unit const char *key, int val);
+void set_unit_val(UNIT *map_unit, char *key, int val);
 void read_config(CONFIG *config,UNIT *map_unit, FILE *f);
 
 #endif
