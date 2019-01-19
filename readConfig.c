a503e32ea06cd1c5ae7e4e63fdde4ea77aa8c706
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-01-19 10:41:03 +0000

    adicionei comentarios na readConfig

--- readConfig.c
+++ readConfig.c
@@ -4,6 +4,12 @@
 #include <stdlib.h>
 
 void set_config_val(CONFIG *config, const char *key, int val) {
+	/**
+	Esta funcao atribui a uma estrutura CONFIG o valor x e y do board
+	@param config = estrutura tipo CONFIG para xdim e ydim
+	@param Key = palavra chave do que se le da INI
+	@param val = valor que se vai atribuir a chave correspondente na estrutura
+	*/
 	if (config == NULL)
 		return;
 	if (strcmp(key, "xdim") == 0)
@@ -14,7 +20,27 @@ void set_config_val(CONFIG *config, const char *key, int val) {
 		;
 }
 
-void read_config(CONFIG *config, FILE *f) {
+void set_unit_val(UNIT *map_unit, const char *key, int val){
+	/**
+	Esta funcao faz o mesmo que a set_config_val mas serve para cada unidade do mapa
+	@param map_unit = Uma estrutura tipo UNIT para guardar os valores das 4 direcoes a volta lidas da INI
+	@param Key = palavra chave do que se le da INI
+	@param val = valor que se vai atribuir a chave correspondente na estrutura
+	*/
+	if (map_unit ==NULL)
+		return;
+	if (strcmp(key, "N")== 0)
+		map_unit->N = val
+}
+
+void read_config(CONFIG *config, UNIT * map_unit, FILE *f) {
+	/**
+	Esta funcao e onde de facto os valores vao ser atribuidos e procurados, quando se quer ler um ficheiro INI so e necessario
+	chamar esta funcao com as estruturas pedidas.
+	@param config = estrutura tipo CONFIG para xdim e ydim
+	@param map_unit = Uma estrutura tipo UNIT para guardar os valores das 4 direcoes a volta lidas da INI
+	@param f = ficheiro INI de onde se vao ler os valores para atribuir as estruturas passadas a funcao
+	*/
 	char str[MAX];
 	char *token;
 	const char *delim = "=\n";
@@ -33,6 +59,7 @@ void read_config(CONFIG *config, FILE *f) {
 			value = atoi(token);
 
 			set_config_val(config, varname, value);
+			
 		}
 	}
 	fclose(f);
--- readconfig.h
+++ readconfig.h
@@ -9,7 +9,17 @@ typedef struct CONFIGURATION {
 	unsigned int xdim;
 	unsigned int ydim;
 } CONFIG;
-void set_config_val(CONFIG *config, const char *key, int val);
-void read_config(CONFIG *config, FILE *f);
+
+typedef struct MAP_UNIT{ 
+/**Estrotura da unidade do mapa, primeira posicao de cada array é uma letra, o resto um numero*/
+	char N[4];
+	char S[4];
+	char W[4];
+	char E[4];
+	char Building;
+}UNIT;
+
+void set_config_val(CONFIG *config, UNIT *map_unit const char *key, int val);
+void read_config(CONFIG *config,UNIT *map_unit, FILE *f);
 
 #endif
