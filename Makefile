376a049b13e29c3eca7a1fcfe3715d3b365d3f0b
Rodrigo Pinheiro <rodpinheiro.12@gmail.com>
2019-01-20 21:54:38 +0000

    Suicidem

--- Makefile
+++ Makefile
@@ -1,23 +1,10 @@
-# Antes de usar fazer: cp Makefile1 Makefile
+Testconfig: testconfig.o ini.o
+		gcc testconfig.o ini.o -o Testconfig
 
-Settlers_of_Japan: main.o diceroll.o fad.o map.o readConfig.o
-	gcc main.o diceroll.o fad.o map_print.o readConfig.o -o Settlers_of_Japan
+testconfig.o: testconfig.c ini.h
+	gcc -Wall -Wextra -std=c99 -g -c -o testconfig.o testconfig.c
 
-main.o: main.c fad.h map.h readconfig.h
-	gcc -Wall -Wextra -std=c99 -g -c -o main.o main.c
-
-diceroll.o: diceroll.c diceroll.h
-	gcc -Wall -Wextra -std=c99 -g -c -o diceroll.o diceroll.c
-
-fad.o: fad.c fad.h
-	gcc -Wall -Wextra -std=c99 -g -c -o fad.o fad.c
-
-map.o: map.h map.c
-	gcc -Wall -Wextra -std=c99 -g -c -o map.o map.c
-
-readConfig.o: readConfig.c readconfig.h
-	gcc -Wall -Wextra -std=c99 -g -c -o readConfig.o readConfig.c
-
+ini.o: ini.c ini.h
+	gcc -Wall -Wextra -std=c99 -g -c -o ini.o ini.c
 clean:
-	rm -f Settlers_of_Japan *.o
-
+		rm -f Testconfig *.o
--- testconfig.c
+++ testconfig.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-#define MAX 32
-typedef struct UNIT { 
-/**Estrutura da unidade do mapa, primeira posicao de cada array é uma letra, o resto um numero*/
-	char Building;
-	char Material;
-	char E;
+#include "stdlib.h"
+#include "ini.h"
+#define MAX 128
+typedef struct{
+/**Estrutura da unidade do mapa, primeira posicao de cada array ï¿½ uma letra, o resto um numero*/
+	char Building[2];
+	char Material[3];
 }UNIT;
 
 typedef struct{
-	UNIT *grid; /**Isto innicializa um apontador que contem UNITS*/
-	unsigned int xdim;
-	unsigned int ydim;
+	int xdim;
+	int ydim;
 	char E[MAX];
 } MAP_CONFIG;
 
@@ -25,15 +24,15 @@ void print_unit(){
         /* Iterate over each column */
         for(int j=1; j<=columns; j++)
         {
-            if(i==1 || i==rows || j==1 || j==columns)
-            {
+        	if(i==1 || i==rows || j==1 || j==columns)
+          {
                 /* Print star for 1st and last row, column */
-                printf("*");
-            }
-            else
-            {
-                printf("X");
-            }
+              printf("*");
+          }
+          else
+          {
+              printf("X");
+          }
         }
         /* Move to the next line */
         printf("\n");
@@ -41,86 +40,16 @@ void print_unit(){
 }
 
 
-void print_map(MAP_CONFIG *config){
-	int xdim = config->xdim;
-	int ydim = config->ydim;
-	for (int i = 0; i < xdim; i++){
-		for(int j = 0; i< ydim; j++);
-			print_unit();
+void print_map(MAP_CONFIG config){
+	for (int i = 0; i < config.xdim; i++){
 		printf("\n");
+		for(int j = 0; i< config.ydim; j++);
+			print_unit();
 	}
-}
-
-FILE *openFile(char *nome, char *mode) {
-	FILE *f;
-	printf("Opening file %s\n", nome);
-	f = fopen(nome, mode);
-
-	if (f == NULL) {
-		fprintf(stderr, "*** It was not possible to open the file %s.", nome);
-		exit(1);	
-	}
-	return f;
-}
-
-void set_config_val(MAP_CONFIG *config, const char *key, char *val) {
-	/**
-	Esta funcao atribui a uma estrutura CONFIG o valor x e y do board
-	@param config = estrutura tipo MAP_CONFIG para xdim e ydim
-	@param Key = palavra chave do que se le da INI
-	@param val = valor que se vai atribuir a chave correspondente na estrutura
-	*/
-
-	if (strcmp(key,"xdim")== 0){
-		config->xdim = 4;
-	}
-	else if (strcmp(key, "ydim") == 0){
-		config->ydim = atoi(val);
-	}
-	else if (strcmp(key, "E")== 0){
-		strcpy(config->E, val);
-	}
-}
-
-MAP_CONFIG read_config(FILE *f) {
-	/**
-	Esta funcao e onde de facto os valores vao ser atribuidos e procurados, quando se quer ler um ficheiro INI so e necessario
-	chamar esta funcao com as estruturas pedidas.
-	@param config = estrutura tipo MAP_CONFIG para xdim e ydim
-	@param map_unit = Uma estrutura tipo UNIT para guardar os valores das 4 direcoes a volta lidas da INI
-	@param f = ficheiro INI de onde se vao ler os valores para atribuir as estruturas passadas a funcao
-	*/
-	MAP_CONFIG config;
-	char str[MAX];
-	char *token;
-	const char *delim = "=\n";
-	char varname[MAX];
-	char value[MAX];
-
-	while (1) {
-		fgets(str, MAX, f);
-		if(feof(f)!= 0) break;
-		if (strchr(str, ';'))
-			;
-		else if (strchr(str, '=')) 
-		{
-			token = strtok(str, delim);
-			strcpy(varname, token);
-			printf("%s\n", varname);
-			token = strtok(NULL, delim);
-			strcpy(value, token);
-			printf("%s\n", value);
-			set_config_val(&config, varname, value);
-		}
-	}
-	
-	fclose(f);
-	return config;
 }
 
 int main(int argc, char **argv) {
 	MAP_CONFIG map;
-	FILE *f;
 	char *filename;
 	for (int i = 0; i < argc; i++)
 		printf("Parametro %d: %s\n", i, argv[i]);
@@ -131,13 +60,35 @@ int main(int argc, char **argv) {
 		printf("A abrir o ficheiro de base...\n"); /**abre ficheiro standard se nao for oferecido como argumento um ficheiro*/
 		filename = "mapa.ini";
 	}
-
-	f = openFile(filename, "r"); /**abre ficheiro atraves de funcao*/
 	/**RETIRAR TODA A INFORMACAO DO FICHEIRO AQUI*/
-	map = read_config(f);
-	printf("%d %d", map.xdim, map.ydim);
-	printf("%s", map.E);
-	map.grid = calloc(map.xdim * map.ydim, sizeof(UNIT)); /**Isto aloca a grid com as estruturas UNIT la dentro*/
+	ini_t *config = ini_load(filename);
+	map.xdim = atoi(ini_get(config,NULL, "xdim"));
+	map.ydim = atoi(ini_get(config,NULL, "ydim"));
+
+	UNIT *grid = calloc((map.xdim * map.ydim), sizeof(UNIT)); /**Isto aloca a grid com as estruturas UNIT la dentro*/
+	char casa[128];
+	char linha[12];
+	for(int i = 0; i<map.ydim; i++){
+
+		sprintf(linha,"linha %d", ((i%map.ydim)+1));
+		for(int j =0; j<map.xdim;j++){
+
+			sprintf(casa,"C%d", ((j%map.xdim)+1));
+			printf(" %s %s\n", linha, casa);
+			ini_sget(config, linha, casa ,"%s", &grid[i*map.xdim + j].Material); //not this
+		}
+	}
+	ini_free(config);
+/**Fecha o ficheiro*/
+
+	printf("%d %d\n", map.xdim, map.ydim);
+	for(int i = 0; i<map.ydim; i++){
+		printf("\n");
+		for(int j =0; j<map.xdim;j++){
+			printf("%s ", grid[i*map.xdim +j].Material);
+		}
+	}
+	print_map(map);
 	getchar();
 	return 0;
 }
